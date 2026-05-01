f87a5c0f2b06e04f5c0a5fdc2d1ae9c143740a5f
edodoyokz <edo.doyokz@gmail.com>
2026-05-01 22:12:30 +0700

    feat: use supabase repository when configured

--- services/router/main.go
+++ services/router/main.go
@@ -6,11 +6,20 @@ import (
 
 	"router/internal/config"
 	"router/internal/httpserver"
+	"router/internal/store"
 )
 
 func main() {
 	cfg := config.Load()
-	server := httpserver.NewWithOptions(httpserver.Options{Config: cfg})
+	var repo store.Repository
+	if cfg.SupabaseURL != "" && cfg.SupabaseServiceKey != "" {
+		repo = store.NewSupabaseRepository(cfg.SupabaseURL, cfg.SupabaseServiceKey, http.DefaultClient)
+		log.Println("router using supabase repository")
+	} else {
+		repo = store.NewMemoryRepository()
+		log.Println("router using memory repository")
+	}
+	server := httpserver.NewWithOptions(httpserver.Options{Config: cfg, Repo: repo, Client: http.DefaultClient})
 
 	addr := ":" + cfg.Port
 	log.Printf("router listening on %s", addr)
