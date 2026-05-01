4eaadcc39eaa3f150c270ca8a46d020f73879f0b
edodoyokz <edo.doyokz@gmail.com>
2026-05-01 21:26:44 +0700

    feat: add router chat contracts and error codes

---
+++ services/router/internal/contracts/errors.go
@@ -0,0 +1,14 @@
+package contracts
+
+const (
+	ErrorInvalidAPIKey             = "invalid_api_key"
+	ErrorWorkspaceNotFound         = "workspace_not_found"
+	ErrorPresetNotFound            = "preset_not_found"
+	ErrorProviderNotFound          = "provider_not_found"
+	ErrorProviderCredentialMissing = "provider_credential_missing"
+	ErrorProviderRequestFailed     = "provider_request_failed"
+	ErrorProviderTimeout           = "provider_timeout"
+	ErrorFallbackExhausted         = "fallback_exhausted"
+	ErrorUnsupportedStreaming      = "unsupported_streaming"
+	ErrorInvalidRequest            = "invalid_request"
+)
